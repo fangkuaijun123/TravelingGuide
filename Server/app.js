421c3a0d5efa77cf0a038c227e5408fa5e28e1e0
cwwei <caiweiwei@whu.edu.cn>
2023-04-05 12:08:26 +0800

    supplement to the firstt running app

--- Server/app.js
+++ Server/app.js
@@ -2,10 +2,19 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.get('/', , function (req, res) {
+    res.sendFile(path.join(__dirname, '/main.html'));
+})
+
+app.get('/main.js', , function (req, res) {
+    res.sendFile(path.join(__dirname, 'js/main.js'));
+})
+
 app.get('/data.json', function (req, res) {
     res.sendFile(path.join(__dirname, '/json/data.json'));
 })
 
-app.listen(80, function () {
+app.listen(8080, function () {
     console.log('Node server is running..');
 });
+//8080 is port number
